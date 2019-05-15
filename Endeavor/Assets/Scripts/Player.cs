046e890849713f5f86855be6fdc9e25379f11700
Jacob Simpson <Jacob.Simpson@ssi-schaefer.us>
2019-05-15 15:09:57 -0400

    User can move the player

--- Endeavor/Assets/Scripts/Player.cs
+++ Endeavor/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour
+public class Player : MovingObject
 {
     static public string[] savedGamePaths;
     static public int currentGameSaveIndex = 0;
@@ -39,6 +39,11 @@ public class Player : MonoBehaviour
         SceneManager.LoadScene(data.currentScene);
     }
 
+    protected override void Start()
+    {
+        base.Start();
+    }
+
     void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
@@ -63,6 +68,34 @@ public class Player : MonoBehaviour
         {
             SavePlayer();
         }
-// This is just For Testing Save Files! close
+        // This is just For Testing Save Files! close
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            // TODO: call AttemptMove with regards to anything the player may interact with
+            AttemptMove<TargetJoint2D>(horizontal, vertical);
+        }
     }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        base.AttemptMove<T>(xDir, yDir);
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        
+    }
+
 }
